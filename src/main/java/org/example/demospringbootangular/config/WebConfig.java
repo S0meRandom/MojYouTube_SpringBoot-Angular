44b1c7715d7996156857534f7fbbf9691e9bf785
Kamil Mazur <kamilosmaz@gmail.com>
2026-03-24 23:25:59 +0100

    Dodano frontend do funkcji przesyłania filmów

---
+++ src/main/java/org/example/demospringbootangular/config/WebConfig.java
@@ -0,0 +1,21 @@
+package org.example.demospringbootangular.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        Path uploadDir = Paths.get("videoFolder");
+        String resourcePath = uploadDir.toAbsolutePath().toUri().toString();
+        registry.addResourceHandler("/videoFolder/**")
+                .addResourceLocations(resourcePath);
+    }
+}
